2931a22dd5509b66f23db03531685302f62898bc
Adrian Rocke <me@adrianrocke.com>
2026-06-17 06:46:01 -0500

    datastar working

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module gbtreader
 
 go 1.26.3
+
+require (
+	github.com/CAFxX/httpcompression v0.0.9 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/starfederation/datastar-go v1.2.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
--- hello.go
+++ hello.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/starfederation/datastar-go/datastar"
+)
 
 func main() {
-		fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+
+	http.HandleFunc("/endpoint", func(w http.ResponseWriter, r *http.Request) {
+		sse := datastar.NewSSE(w, r)
+		sse.PatchElements(
+			`<div id="hal">I’m sorry, Dave. I’m afraid I can’t do that.</div>`,
+		)
+
+		time.Sleep(1 * time.Second)
+
+		sse.PatchElements(
+			`<div id="hal">Waiting for an order...</div>`,
+		)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		fmt.Fprintf(w, `
+<DOCTYPE html>
+<html>
+	<head>
+		<script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.2/bundles/datastar.js"></script>
+	</head>
+	<body>
+		<button data-on:click="@get('/endpoint')">
+			Open the pod bay doors, HAL.
+		</button>
+		<div id="hal"></div>
+	</body>
+</html>
+`)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
