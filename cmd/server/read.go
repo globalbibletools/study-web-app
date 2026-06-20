05d3d7e11c7adc8ed673808caf82e00f247ad332
Adrian Rocke <me@adrianrocke.com>
2026-06-19 21:55:33 -0500

    render chapter text

--- cmd/server/read.go
+++ cmd/server/read.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"crawshaw.io/sqlite/sqlitex"
+	"github.com/starfederation/datastar-go/datastar"
 	. "maragu.dev/gomponents"
 	ds "maragu.dev/gomponents-datastar"
 	. "maragu.dev/gomponents/components"
@@ -17,12 +20,17 @@ type Signals struct {
 	Chapter string `json:"chapter"`
 }
 
+var dbpool *sqlitex.Pool
+
 func main() {
-	dbpool, err := sqlitex.Open("file:memory:?mode=memory", 0, 10)
+	var err error
+	dbpool, err = sqlitex.Open("file:export.db", 0, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = dbpool
+
+	fileServer := http.FileServer(http.Dir("./web"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	http.HandleFunc("/chapter/{chapterNumber}", func(w http.ResponseWriter, r *http.Request) {
 		chapter, err := strconv.ParseUint(r.PathValue("chapterNumber"), 10, 32)
@@ -31,55 +39,208 @@ func main() {
 			return
 		}
 
-		if err := read(uint(chapter)).Render(w); err != nil {
+		chapterData, err := getChapterData(r.Context(), uint(chapter))
+		if err != nil {
 			log.Printf("Error: %v\n", err)
-			return
+			http.Error(w, "Server Error", http.StatusInternalServerError)
 		}
+
+		sse := datastar.NewSSE(w, r)
+
+		sse.PatchElementGostar(
+			pageContent(chapterData),
+			datastar.WithMode(datastar.ElementPatchModeReplace),
+		)
+		sse.PatchElementGostar(
+			toolbar(chapterData),
+		)
+
+		chapterStr := strconv.FormatUint(uint64(chapter), 10)
+		sse.MarshalAndPatchSignals(Signals{ Chapter: chapterStr, })
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_ = read(1).Render(w)
+		chapterData, err := getChapterData(r.Context(), 1)
+		if err != nil {
+			log.Printf("Error: %v\n", err)
+			http.Error(w, "Server Error", http.StatusInternalServerError)
+		}
+
+		_ = read(chapterData).Render(w)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func pageContent(page string) Node {
-	return Div(ID("content"), Text("Chapter "), Text(page))
+type WordData struct {
+	id   string
+	text string
 }
 
-func chapterInput() Node {
-	return Input(ID("chapter-input"), ds.Bind("chapter"))
+type VerseData struct {
+	verseNumber uint
+	words       []WordData
 }
 
-func read(page uint) Node {
-	pageStr := strconv.FormatUint(uint64(page), 10)
+type ChapterData struct {
+	BookName string
+	Chapter  uint
+	Verses   []VerseData
+}
 
-	return layout(
+func getChapterData(context context.Context, chapter uint) (ChapterData, error) {
+	conn := dbpool.Get(context)
+	if conn == nil {
+		return ChapterData{}, fmt.Errorf("failed to get database connection")
+	}
+	defer dbpool.Put(conn)
+
+	stmt := conn.Prep(`
+		select verse.number as verse, word.id, word.text from verse
+		join word on word.verse_id = verse.id
+		where book_id = $bookId
+		and chapter = $chapter
+		order by verse.id, word.id
+	`)
+	stmt.SetInt64("$bookId", 1)
+	stmt.SetInt64("$chapter", int64(chapter))
+
+	var verses []VerseData
+	var verseNumber uint = 0
+	var words []WordData
+	for {
+		if hasRow, err := stmt.Step(); err != nil {
+			return ChapterData{}, err
+		} else if !hasRow {
+			break
+		}
+
+		newVerseNumber := uint(stmt.GetInt64("verse"))
+		if newVerseNumber != verseNumber {
+			if verseNumber > 0 {
+				verses = append(verses, VerseData{
+					verseNumber,
+					words,
+				})
+			}
+			verseNumber = newVerseNumber
+			verses = []VerseData{}
+		}
+
+		words = append(words, WordData{
+			id:   stmt.GetText("id"),
+			text: stmt.GetText("text"),
+		})
+	}
+
+	if len(words) > 0 {
+		verses = append(verses, VerseData{
+			verseNumber,
+			words,
+		})
+	}
+
+	stmt = conn.Prep(`
+		select name from book
+		where id = $bookId
+	`)
+	stmt.SetInt64("$bookId", 1)
+
+	if hasRow, err := stmt.Step(); err != nil {
+		return ChapterData{}, err
+	} else if !hasRow {
+		return ChapterData{}, nil
+	}
+	stmt.Step()
+
+	return ChapterData{
+		BookName: stmt.GetText("name"),
+		Chapter:  chapter,
+		Verses:   verses,
+	}, nil
+}
+
+func pageContent(data ChapterData) Node {
+	return Div(
+		ID("content"),
 		Div(
-			ds.Signals(map[string]any{
-				"chapter": pageStr,
-			}),
-			chapterInput(),
-			Button(
-				ds.On("click", fmt.Sprintf("@get('/chapter/%d')", page-1)),
-				Text("Prev Chapter"),
-			),
-			Button(
-				ds.On("click", fmt.Sprintf("@get('/chapter/%d')", page+1)),
-				Text("Next Chapter"),
+			Class("reading-content"),
+			If(data.Chapter < 40, Dir("rtl")),
+			If(data.Chapter >= 40, Dir("ltr")),
+			P(
+				Map(data.Verses, func(verse VerseData) Node {
+					verseNumberStr := strconv.FormatUint(uint64(verse.verseNumber), 10)
+					return Span(
+						Span(
+							Class("verse-number"),
+							Text(verseNumberStr),
+							Text(" "),
+						),
+						Map(verse.words, func(word WordData) Node {
+							return Group([]Node{
+								Span(Text(word.text)),
+								If(!strings.HasSuffix(word.text, "־"), Text(" ")),
+							})
+						}),
+					)
+				}),
 			),
 		),
-		pageContent(pageStr),
 	)
 }
 
-func layout(children ...Node) Node {
+func chapterInput(data ChapterData) Node {
+	return Div(
+		ID("chapter-input"),
+		Class("chapter-input"),
+		Input(
+			Class("text-input"),
+			ds.Bind("chapter"),
+		),
+		Div(
+			Class("chapter-input-actions"),
+			Button(
+				ds.On("click", fmt.Sprintf("@get('/chapter/%d')", data.Chapter-1)),
+				Text("P"),
+			),
+			Button(
+				ds.On("click", fmt.Sprintf("@get('/chapter/%d')", data.Chapter+1)),
+				Text("N"),
+			),
+		),
+	)
+}
+
+func toolbar(data ChapterData) Node {
+	return Div(
+		ID("toolbar"),
+		chapterInput(data),
+	)
+}
+
+func read(data ChapterData) Node {
+	pageStr := strconv.FormatUint(uint64(data.Chapter), 10)
+
+	return layout(
+		"/static/css/read.css",
+		ds.Signals(map[string]any{
+			"chapter": pageStr,
+		}),
+		toolbar(data),
+		pageContent(data),
+	)
+}
+
+func layout(
+	styles string,
+	children ...Node,
+) Node {
 	return HTML5(HTML5Props{
 		Title:    "Global Bible Tools",
 		Language: "en",
 		Head: []Node{
 			Script(Type("module"), Src("https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.2/bundles/datastar.js")),
+			If(len(styles) > 0, Link(Rel("stylesheet"), Href(styles))),
 		},
 		Body: children,
 	})
--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
             gopls
             gotools
             golangci-lint
+            sqlite
           ];
 
         };
